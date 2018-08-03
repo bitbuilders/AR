0cec04f5d60dd869f1736714d691aa756d42d462
Colin Misbach <colin@misbach.com>
2018-08-03 11:59:38 -0600

    Getting ground plane working

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 velocity = Vector3.zero;
+        velocity.y = Mathf.PingPong(Time.time * 0.5f, 0.5f);
+
+        transform.position = velocity;
+    }
+}
