02f057ccdf5805c9ce815668d4442b919f047c29
Colin Misbach <colin@misbach.com>
2018-08-09 23:05:51 -0600

    Finished but doesn't work on android

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,22 +5,44 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] [Range(0.0f, 20.0f)] float m_speed = 1.0f;
+    [SerializeField] [Range(0.0f, 1000.0f)] float m_healthGrow = 100.0f;
+    [SerializeField] Color m_endColor;
+    public float Health { get; set; }
 
     Vector3 m_startPos;
     Camera m_camera;
+    Material m_material;
+    [SerializeField] Color m_startColor;
 
     void Start()
     {
         m_startPos = transform.position;
         m_camera = Camera.main;
+        Health = 1000.0f;
+        m_material = GetComponentInChildren<Renderer>().material;
+        m_startColor = m_material.GetColor("_BaseColorFactor");
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 dir = m_camera.transform.position - transform.position;
-        Vector3 velocity = dir.normalized * m_speed;
+        //Vector3 dir = m_camera.transform.position - transform.position;
+        //Vector3 velocity = dir.normalized * m_speed;
 
-        transform.position += velocity;
+        //transform.position += velocity;
+
+        float a = Health / 1000.0f;
+        m_material.SetColor("_BaseColorFactor", Color.Lerp(m_endColor, m_startColor, a));
+        Health += m_healthGrow * Time.deltaTime;
+        Health = Mathf.Clamp(Health, 0.0f, 1000.0f);
+    }
+
+    private void OnApplicationQuit()
+    {
+        m_material.SetColor("_BaseColorFactor", m_startColor);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
     }
 }
---
+++ Assets/Scripts/Lazer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lazer : MonoBehaviour {
+
+    [SerializeField] GameObject m_lazerHit1 = null;
+    [SerializeField] GameObject m_lazerHit2 = null;
+    // Use this for initialization
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    }
+
+
+    private void OnParticleTrigger()
+    {
+        //print("hi");
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        Vector3 dir = transform.position - other.transform.position;
+        if (other.GetComponent<MeshCollider>() != null)
+        {
+            m_lazerHit1.transform.position = other.transform.position;
+            m_lazerHit1.transform.rotation = other.transform.rotation;
+        }
+        else
+        {
+            other.GetComponent<Enemy>().Health = other.GetComponent<Enemy>().Health - 8.0f;
+            m_lazerHit2.transform.position = other.transform.position;
+            m_lazerHit2.transform.rotation = Quaternion.Euler(-90.0f, 0.0f, 0.0f);
+            m_lazerHit2.transform.rotation = other.transform.rotation * m_lazerHit2.transform.rotation;
+        }
+    }
+}
